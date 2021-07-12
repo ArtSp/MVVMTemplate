94c9dfcd518a2d82e32de06183fba161c6fbabd4
Artjoms Spole <artjoms.spole@mediapark.com>
2021-07-12 12:10:24 +0300

    example

--- MVVMTemplate/Helpers/Framework/KeyPath+Ex.swift
+++ MVVMTemplate/Helpers/Framework/KeyPath+Ex.swift
@@ -4,6 +4,29 @@
 
 import Foundation
 
+
+
+/// Keypath setter for closures
+///
+/// Example in `test()`:
+///
+///     class Foo {
+///         var title: String?
+///         var description: String?
+///
+///         static let shared = Foo()
+///         private init() {}
+///
+///         private func request(text: String, completion: @escaping (String) -> Void) {
+///             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+///                 completion(text)
+///             }
+///         }
+///         func test() {
+///             request(text: "Title", completion: setter(keyPath: \.title , on: self))
+///             request(text: "Description", completion: setter(keyPath: \.description , on: self))
+///         }
+///   }
 func setter<Object: AnyObject, Value>(
     keyPath: ReferenceWritableKeyPath<Object, Value>,
     on object: Object
